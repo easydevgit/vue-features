a54175528164d32a281cd19373bbd2034305fc36
easydevgit <urserg1@gmail.com>
2022-01-27 17:58:23 +0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <Places>
+    <template #country="{id, place, log}">
+      {{id}}{{place}}
+
+      <button @click="log">Click</button>
+    </template>
+
+    <template #city="{id, place}">
+      {{id}}{{place}}
+    </template>
+  </Places>
+</template>
+
+<script>
+import Places from "./components/Places.vue";
+
+export default {
+  name: "App",
+  components: {
+    Places,
+  },
+};
+</script>
---
+++ src/components/Places.vue
@@ -0,0 +1,32 @@
+<template>
+  <div
+    v-for="place in places"
+    :key="place.city"
+  >
+    <slot name="country" :place="place.country" id="1" :log="logPlaces"> </slot>
+    <slot name="city" :place="place.city" id="2"> </slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Places",
+  setup() {
+    const places = [
+      { country: "GB", city: "London" },
+      { country: "IT", city: "Rome" },
+      { country: "USA", city: "New York" },
+    ];
+
+    const logPlaces = () => {
+      console.log('places', places);
+    }
+
+
+    return {
+      places,
+      logPlaces
+    };
+  },
+};
+</script>
